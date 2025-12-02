461ae8971d7157649f52a6f4692a4cbb46b2853d
Suraj-kashyap01 <surajkashyap0022@gmail.com>
2025-12-02 10:16:07 +0530

    Fix PORT undefined issue

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: "http://localhost:5173"  // frontend ka URL
