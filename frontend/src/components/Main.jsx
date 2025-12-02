0aa59ba52de0c2754d005a31a09a646b777fffaf
Suraj-kashyap01 <surajkashyap0022@gmail.com>
2025-12-02 12:27:40 +0530

    style change

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -2,17 +2,19 @@ const Main = () => {
   return (
     <div
       className="
-        h-[85vh] w-full bg-cover bg-center bg-no-repeat
+        h-[85vh] w-full
+        bg-cover bg-center bg-no-repeat
         bg-[url('https://pioneereventshq.com/wp-content/uploads/2024/05/Centerpieces-Candle-Holders.jpg')]
-        
         md:bg-[url('https://pioneereventshq.com/wp-content/uploads/2024/05/WhatsApp-Image-2024-04-04-at-9.35.54-AM-1.jpeg')]
         md:hover:bg-[url('https://pioneereventshq.com/wp-content/uploads/2024/05/lantern.webp')]
-        transition-all duration-700 ease-in-out"
+        transition-all duration-700 ease-in-out
+        flex items-start justify-start
+        px-8 pt-20
+      "
     >
-      <h1 className="md:hidden font-poppins text-[60px] font-extrabold text-white pt-35 pl-30">Centerpieces</h1>
+     
     </div>
   );
 };
-
 
 export default Main;
